e90b5b21f28450719ff2b3e11d75ef106dfb4bf4
Jack <xhzengAIB@gmail.com>
2014-06-15 16:56:48 +0000

    Init project.

---
+++ JackFastKit/JackFastKit/JackFastKit/JackFastKit.h
@@ -0,0 +1,13 @@
+//
+//  JackFastKit.h
+//  JackFastKit
+//
+//  Created by 曾 宪华 on 14-6-15.
+//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JackFastKit : NSObject
+
+@end
---
+++ JackFastKit/JackFastKit/JackFastKit/JackFastKit.m
@@ -0,0 +1,21 @@
+//
+//  JackFastKit.m
+//  JackFastKit
+//
+//  Created by 曾 宪华 on 14-6-15.
+//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//
+
+#import "JackFastKit.h"
+
+@implementation JackFastKit
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
